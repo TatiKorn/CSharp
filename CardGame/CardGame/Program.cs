423ad776b040eb6c1fc88597ab2dbd0dcc045e4a
Tatiana Kornilova <Tatiana.Kornilova@acumatica.com>
2023-09-21 14:07:23 -0500

    Add project files.

---
+++ CardGame/CardGame/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace CardGame
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json.Linq;
+
+    class Program
+    {
+        static async Task Main()
+        {
+            // Step 1: Navigate to https://deckofcardsapi.com/
+            string baseUrl = "https://deckofcardsapi.com/";
+
+            // Step 2: Confirm the site is up
+            using (HttpClient httpClient = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage siteResponse = await httpClient.GetAsync(baseUrl);
+                    if (!siteResponse.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Deck of Cards API site is not available.");
+                        return;
+                    }
+
+                    // Step 3: Get a new deck
+                    HttpResponseMessage newDeckResponse = await httpClient.GetAsync(baseUrl + "api/deck/new/");
+                    if (!newDeckResponse.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Failed to create a new deck.");
+                        return;
+                    }
+
+                    string deckJson = await newDeckResponse.Content.ReadAsStringAsync();
+                    JObject deckData = JObject.Parse(deckJson);
+                    string deckId = deckData["deck_id"].ToString();
+
+                    // Step 4: Shuffle the deck
+                    HttpResponseMessage shuffleResponse = await httpClient.GetAsync(baseUrl + $"api/deck/{deckId}/shuffle/");
+                    if (!shuffleResponse.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Failed to shuffle the deck.");
+                        return;
+                    }
+
+                    // Step 5: Deal three cards to each of two players
+                    HttpResponseMessage dealResponse = await httpClient.GetAsync(baseUrl + $"api/deck/{deckId}/draw/?count=6");
+                    if (!dealResponse.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Failed to deal cards.");
+                        return;
+                    }
+
+                    string cardsJson = await dealResponse.Content.ReadAsStringAsync();
+                    JObject cardsData = JObject.Parse(cardsJson);
+                    JArray cards = (JArray)cardsData["cards"];
+
+                    // Step 6: Check whether either has blackjack
+                    bool player1HasBlackjack = HasBlackjack(cards.Take(3));
+                    bool player2HasBlackjack = HasBlackjack(cards.Skip(3).Take(3));
+
+                    // Step 7: If either has, write out which one does
+                    if (player1HasBlackjack)
+                    {
+                        Console.WriteLine("Player 1 has blackjack!");
+                    }
+                    if (player2HasBlackjack)
+                    {
+                        Console.WriteLine("Player 2 has blackjack!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        // Blackjack checking logic
+        static bool HasBlackjack(IEnumerable<JToken> playerCards)
+        {
+            return false; // Placeholder logic
+        }
+    }
+}
---
+++ CheckersGame/CheckersGame/Program1.cs
@@ -0,0 +1,63 @@
+﻿using SeleniumExtras.WaitHelpers;
+
+namespace CheckersGame
+{
+    using System;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Support.UI;
+
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            // Step 1: Initialize the ChromeDriver
+            IWebDriver driver = new ChromeDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            // Step 2: Navigate to the website
+            driver.Navigate().GoToUrl("https://www.gamesforthebrain.com/game/checkers/");
+
+            // Step 2: Confirm that the site is up
+            if (driver.Title.Contains("Checkers"))
+            {
+                Console.WriteLine("Website is up!");
+            }
+            else
+            {
+                Console.WriteLine("Website is not responding.");
+            }
+
+            // Step 3: Make five legal moves as orange
+            // Perform the move by clicking the orange piece and then clicking a legal move
+            IWebElement orangePiece = driver.FindElement(By.Name("space62"));
+            orangePiece.Click();
+            IWebElement legalMove = driver.FindElement(By.ClassName("space53"));
+            legalMove.Click();
+            IWebElement makeAMove = driver.FindElement(By.Id("message"));
+            // Wait for the element to be displayed
+            wait.Until(ExpectedConditions.ElementIsVisible(By.Id("message")));
+            // Click the button
+            makeAMove.Click();
+
+
+            // Step 3c: Restart the game after five moves
+            IWebElement restartButton = driver.FindElement(By.LinkText("Restart"));
+            restartButton.Click();
+
+            // Step 3d: Confirm that the restarting was successful
+            if (driver.Title.Contains("Checkers") && makeAMove.Text.Equals("Select an orange piece to move."))
+            {
+                Console.WriteLine("Game has been successfully restarted!");
+            }
+            else
+            {
+                Console.WriteLine("Game restart failed.");
+            }
+
+            // Close the browser
+            driver.Quit();
+        }
+    }
+
+}
